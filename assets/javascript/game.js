007f3660c85efc31725e42055fb26c5d731d318f
HelloBetafish <bethany.pfeister@gmail.com>
2017-08-08 16:26:02 -0700

    reworked the random number logic for no repeat numbers

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,37 +1,50 @@
 $(document).ready(function(){
   
   var targetNumber = Math.floor((Math.random() * 121) + 19);
-  $("#target").text(targetNumber);
-
   var counter = 0;
   var wins = 0;
   var losses = 0;
   var gameOver = false;
-  
-  $("#fluidCount").text(counter);
+  var num=[];
+  $("#target").text(targetNumber);
   $("#wins").text(wins);
   $("#losses").text(losses);
+  $("#fluidCount").text(counter);
   
   $("#flip").click(function(){
     $("#instructions").slideToggle(2000);
   });
-  
- function assignRandomNumbers(){
-    var randomNumber = Math.floor((Math.random() * 13) + 1);
-	$("#ruby").attr("bird-value", randomNumber);
-	randomNumber = Math.floor((Math.random() * 13) + 1);
-	$("#sapphire").attr("bird-value", randomNumber);
-	randomNumber = Math.floor((Math.random() * 13) + 1);
-	$("#emerald").attr("bird-value", randomNumber);
-	randomNumber = Math.floor((Math.random() * 13) + 1);
-	$("#topaz").attr("bird-value", randomNumber);
+
+  function assignRandomNumbers(){
+  	for (i=0; i<4; i++){
+  	  num[i]=Math.floor((Math.random() * 13) + 1);
+  	  if (num[1]==num[0]){
+  	  	while (num[1]==num[0]){
+  	  	  num[1]=Math.floor((Math.random() * 13) + 1);
+  	  	}
+  	  }
+  	  if(num[2]==num[1] || num[2]==num[0]){
+  	  	while (num[2]==num[1] || num[2]==num[0]){
+  	  	  num[2]=Math.floor((Math.random() * 13) + 1);
+  	  	}
+  	  }
+  	  if (num[3]==num[2] || num[3]==num[1] || num[3]==num[0]){
+  	  	while (num[3]==num[2] || num[3]==num[1] || num[3]==num[0]){
+  	  	  num[3]=Math.floor((Math.random() * 13) + 1);
+  	  	}
+  	  }
+  	}
+	$("#ruby").attr("bird-value", num[0]);
+	$("#sapphire").attr("bird-value", num[1]);
+	$("#emerald").attr("bird-value", num[2]);
+	$("#topaz").attr("bird-value", num[3]);
   }
   
   assignRandomNumbers();
 
  $(".bird").on("click", function(){
+
 	if (gameOver === false){
-	
 	  var birdValue = ($(this).attr("bird-value"));
 	  birdValue = parseInt(birdValue);
 	  counter += birdValue;
@@ -65,14 +78,14 @@ $(document).ready(function(){
 	  $("#fluidCount").text(counter);
 	  targetNumber = Math.floor((Math.random() * 121) + 19);
 	  $("#target").text(targetNumber);
+	  num=[];
 	  assignRandomNumbers();
-	  gameOver = false;
 	  $("#winMessage").hide();
 	  $("#lostMessage").hide();
 	  $(".progress-bar").css('width',0+"%").attr('aria-valuenow',0);
 	  $(".progress-bar").text(0+"%");
 	  $(".progress-bar").removeClass("progress-bar-danger progress-bar-success");
 	  $(".progress-bar").addClass("progress-bar-info");
-
+	  gameOver = false;
 	});
 });
